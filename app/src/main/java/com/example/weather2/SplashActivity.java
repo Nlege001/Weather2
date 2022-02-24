f004c17b6a436ca474ec903ada6c387be1fd8beb
Naol Legesse <nlege001@plattsburgh.edu>
2022-02-24 15:40:33 -0600

    finalized

---
+++ app/src/main/java/com/example/weather2/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.weather2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private Animation slideAnimation;
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+        slideAnimation = AnimationUtils.loadAnimation(this, R.anim.animate);
+        logo.startAnimation(slideAnimation);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
